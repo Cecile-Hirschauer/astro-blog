2ad7cc2ef9be4a8784876afa47f58d6704cc8ee5
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-19 22:50:12 +0200

    Add constants for SITE_TITLE, SITE_DESCRIPTION, HOMEPAGE_ARTCLE_LIMIT and ARTICLES_PER_PAGE

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION =
+  "Articles, stories and tutorials for from Tech People";
+export const HOMEPAGE_ARTCLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
